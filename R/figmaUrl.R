401f3d97db0074d18bbfc5c4d8da1a5401112b2d
Martin老師 <tpemartin@gmail.com>
2023-03-10 06:58:30 +0800

    Initial commit

---
+++ R/figmaUrl.R
@@ -0,0 +1,49 @@
+figmaAccessToken=Sys.getenv("figmaApiKey")
+httr::config(
+  httr::add_headers(
+    `X-Figma-Token`=figmaAccessToken
+  )
+)
+
+figmaUrl = "https://www.figma.com/file/9GQBb1dc5aYFqWj0w06jMx/Taitung-Tourism?node-id=16%3A39&t=pextNDclqsiGEIOX-0"
+keyPattern <- "(?<=(https://www.figma.com/file/))[^/]+"
+
+key = stringr::str_extract(
+  figmaUrl, keyPattern
+)
+filePattern =
+  paste0(
+    glue::glue("(?<=(https://www.figma.com/file/{key}/))"),
+    "[^/?]+")
+.file = stringr::str_extract(
+  figmaUrl, filePattern
+)
+queryPattern =
+  paste0(
+    glue::glue("(?<=(https://www.figma.com/file/{key}/{.file}\\?))"),
+    "[^?]+"
+  )
+query= stringr::str_extract(
+  figmaUrl, queryPattern
+)
+query |>
+  stringr::str_split("&") |>
+  unlist() |>
+  stringr::str_split("=") -> querySplit
+purrr::map(querySplit, ~{.x[[1]]}) |> unlist() -> queryParams
+purrr::map(querySplit, ~{.x[[2]]}) -> queryValues
+info = list(
+  key=key, file=.file, query=setNames(queryValues, queryParams)
+)
+
+
+.endpoint = glue::glue("https://api.figma.com/v1/files/{info$key}") |>
+  paste0("?ids=",info$query$`node-id`)
+.endpoint
+httr::GET(
+  url=.endpoint,
+  httr::add_headers(
+    `X-Figma-Token`=figmaAccessToken
+  )
+) -> request
+httr::content(request) -> nodeInfo
