8709b800f8650903c00994d83fb9b2ae523ac3a0
Martin老師 <tpemartin@gmail.com>
2023-03-16 15:31:41 +0800

    beta release

--- R/figmaUrl.R
+++ R/figmaUrl.R
@@ -1,9 +1,40 @@
-
-
-figmaUrl0 = "https://www.figma.com/file/9GQBb1dc5aYFqWj0w06jMx/Taitung-Tourism?node-id=16%3A39&t=pextNDclqsiGEIOX-0"
-
+#' Create Div generator from a link to Figma frame
+#'
+#' @return an environment to show raw response from Figma. Other than that a "page" object will be created in the global environment
 Figma = function(){
   fig = new.env()
+  getNodeInfoFromFigmaUrlInClipboard() -> info
+  info$document$children |>
+    purrr::map(~{.x$name}) -> pageNames
+  info$document$children |> setNames(pageNames)-> pages
+
+
+  page = list()
+  for(i in seq_along(pageNames)){
+    page_i = pageNames[[i]]
+    page[[page_i]] = list(
+      frame = list()
+    )
+    pages[[i]]$children |> purrr::map_chr(~{.x$name}) -> targetFrame
+    if(length(targetFrame)==0) next
+    page[[page_i]]$frame[[targetFrame]] <- new.env()
+    page[[page_i]]$frame[[targetFrame]]$classname = targetFrame
+    # Each page only has one child i.e. targetFrame
+    page[[page_i]]$frame[[targetFrame]]$childrenElements =
+      pages[[page_i]]$children[[1]]$children
+
+    # populate targetFrameEnv
+    # debug(populateFrameEnv)
+    page[[page_i]]$frame[[targetFrame]] |>
+      populateFrameEnv()
+
+    if(length(targetFrame)!=0){
+      page[[page_i]]$frame[[targetFrame]] |> populateSeparateDiv()
+      page[[page_i]]$frame[[targetFrame]] |> refineEnvDiv()
+    }
+  }
+    .GlobalEnv$page <- page
+
   fig$getDiv = function(){
     getNodeInfoFromFigmaUrlInClipboard() -> fig$.nodeInfo
   }
@@ -63,7 +94,7 @@ getNodeInfoFromFigmaUrl <- function(figmaUrl="https://www.figma.com/file/9GQBb1d
 
   return(nodeInfo)
 }
-populateChildElementsForInfoList_i <- function(infoList_i) {
+populateChildElementsForInfoList_i0 <- function(infoList_i) {
 
   if(length(infoList_i$childrenElements)==0) return()
 
@@ -99,4 +130,211 @@ populateChildElementsForInfoList_i <- function(infoList_i) {
   }
 
   return(infoListEnv)
+}
+populateChildElementsForInfoList_i <- function(infoList_i) {
+
+  if(length(infoList_i$childrenElements)==0) return()
+
+  infoListEnv = list(
+    childElements = infoList_i$childrenElements
+  )
+  infoList_i$childrenElements -> childrenElements
+  childrenElements |>
+    purrr::map_chr(~{.x$name}) -> majorFrameNames
+
+  elementChildrenList=vector("list", length(childrenElements)) |>
+    setNames(majorFrameNames)
+
+  childrenDiv = list()
+  for(i in seq_along(majorFrameNames)){
+    childrenElement_i = childrenElements[[i]]
+    infoList_i[[majorFrameNames[[i]]]] = new.env()
+    infoList_i[[majorFrameNames[[i]]]]$childrenElements =
+      childrenElement_i$children
+    infoList_i[[majorFrameNames[[i]]]]$classname = majorFrameNames[[i]]
+    infoList_i[[majorFrameNames[[i]]]]$div = function(...) {
+      htmltools::div(
+        class=infoList_i[[majorFrameNames[[i]]]]$classname,
+        ...
+      )
+    }
+
+    childrenDiv = append(
+      childrenDiv, list(infoListEnv[[childrenElements[[i]]$name]]$div())
+    )
+  }
+
+  infoListEnv$div = function(...) {
+    htmltools::div(
+      ...,
+      childrenDiv
+    )
+  }
+
+  return(infoListEnv)
+}
+
+populateParentList0 <- function(parentList) {
+  parentChildrenNames = purrr::map_chr(
+    parentList$childrenElements, ~{.x$name}
+  )
+
+  for(i in seq_along(parentChildrenNames)){
+    child_i = parentList$childrenElements[[i]]
+    # if(length(child_i)==0) next
+    child_i_env = new.env()
+    child_i_env$childrenElements = child_i$children
+
+    child_i_list = list(
+      childrenElements = child_i$children
+    )
+    if(length(child_i$children)!=0){
+      grandChildNames = child_i_list$childElements |>
+        purrr::map_chr(~{.x$name})
+
+      child_i_list |>
+        populateChildElementsForInfoList_i() ->
+        child_i_list
+      child_i_div = child_i_list$div
+      child_i_list$div = function(...) {
+        child_i_div(class=parentChildrenNames[[i]])
+      }
+      parentList[[parentChildrenNames[[i]]]] = child_i_list
+    } else {
+      parentList[[parentChildrenNames[[i]]]]$div = function(...){
+        htmltools::div(class=parentChildrenNames[[i]])
+      }
+    }
+
+  }
+
+  return(parentList)
+}
+populateParentList <- function(parentList) {
+  parentChildrenNames = purrr::map_chr(
+    parentList$childrenElements, ~{.x$name}
+  )
+
+  i = 1
+  length(parentList)
+  envName = parentList$childrenElements[[i]]$name
+  parentList[[envName]] = new.env()
+  parentList[[envName]]$classname = envName
+  parentList[[envName]]$childrenElements =
+  debug(populateChildElementsForInfoList_i)
+  parentList |>
+    populateChildElementsForInfoList_i() -> child_i_list
+  parentList$elements |> class()
+
+  for(i in seq_along(parentChildrenNames)){
+    child_i = parentList$childrenElements[[i]]
+    # if(length(child_i)==0) next
+    child_i_env = new.env()
+    child_i_env$childrenElements = child_i$children
+
+    # child_i_list = list(
+    #   childrenElements = child_i$children
+    # )
+    if(length(child_i$children)!=0){
+      grandChildNames = child_i_env$childElements |>
+        purrr::map_chr(~{.x$name})
+
+      child_i_env |>
+        populateChildElementsForInfoList_i() ->
+        child_i_env
+      child_i_div = child_i_env$div
+      child_i_env$div = function(...) {
+        child_i_div(class=parentChildrenNames[[i]])
+      }
+      parentList[[parentChildrenNames[[i]]]] = child_i_env
+    } else {
+      parentList[[parentChildrenNames[[i]]]]$div = function(...){
+        htmltools::div(class=parentChildrenNames[[i]])
+      }
+    }
+
+  }
+
+
+  return(parentList)
+}
+obtainChildrenNames <- function(parentList) {
+  parentList |> names() -> listNames
+  listNames |>
+    setdiff(
+      c("childrenElements", "div", "classname")
+    )
+}
+populateFrameEnv = function(frameEnv){
+  if(length(frameEnv$childrenElements)==0) return()
+  for(i in seq_along(frameEnv$childrenElements)){
+    # i=1
+    childName = frameEnv$childrenElements[[i]]$name
+    frameEnv[[childName]] = new.env()
+    childFrame = frameEnv[[childName]]
+    childFrame$childrenElements = frameEnv$childrenElements[[i]]$children
+    childFrame$classname = childName
+    populateFrameEnv(childFrame)
+  }
+}
+refineEnvDiv <- function(tf) {
+  tf |> obtainChildrenNames() -> childEnvNames
+  tf$div = function(...){
+    divList = list()
+    for(i in seq_along(childEnvNames)){
+      divList = append(divList, list(tf[[childEnvNames[[i]]]]$div()))
+    }
+    htmltools::div(
+      ...,
+      divList,
+      class=tf$classname
+    )
+  }
+
+  for(i in seq_along(childEnvNames)){
+    tf[[childEnvNames[[i]]]] |> refineEnvDiv()
+  }
+}
+populateSeparateDiv <- function(targetFrame){
+  targetFrame |> obtainChildrenNames() -> childNames
+
+  targetFrame$div = function(...){
+    htmltools::div(
+      ..., class=classname
+    )}
+  environment(targetFrame$div) <- targetFrame
+
+  for(i in seq_along(childNames)){
+    populateSeparateDiv(targetFrame[[childNames[[i]]]])
+  }
+
+}
+populateEnvDiv <- function(targetFrame) {
+  targetFrame |> obtainChildrenNames() -> childNames
+  if(length(childNames)==0){
+    targetFrame$div = function(...){
+      htmltools::div(
+        ..., class=classname
+      )}
+    environment(targetFrame$div) <- targetFrame
+  } else {
+    for(i in seq_along(childNames)) populateEnvDiv(targetFrame[[childNames[[i]]]])
+    targetFrame$div <- function(...) {
+      rlang::current_env() |> rlang::env_parent() -> pe
+      # browser()
+      # targetFrame |> obtainChildrenNames() -> childNames
+      divList <- list()
+      for (i in seq_along(childNames)) {
+        divList <- append(divList, list(
+          pe[[childNames[[i]]]]$div()))
+      }
+      htmltools::div(
+        ...,
+        divList,
+        class = classname
+      )
+    }
+    environment(targetFrame$div) <- targetFrame
+  }
+
 }
