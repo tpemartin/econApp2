444d3e49e095fa57df48f5e9eab891af2a99fa47
Martin老師 <tpemartin@gmail.com>
2023-03-23 18:05:52 +0800

    add dependency

--- test/Untitled.R
+++ test/Untitled.R
@@ -1,12 +1,1 @@
 econApp2::Figma()
-page$Martin$frame$進入到祝福$app$div()
-page$Martin$frame$進入到祝福$app$css()
-
-component = page$Martin$frame$進入到祝福$app
-
-
-getCssFromComponent()
-
-if(targetFrame$type=="TEXT")
-
-targetFrame$characters
